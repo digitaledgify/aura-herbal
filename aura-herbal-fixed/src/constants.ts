399c46fbe7a01292f77f72759def3d9f0d8acb57
digitaledgify <digitaledgify@gmail.com>
2026-07-06 17:35:45 +0530

    Delete aura-herbal-fixed directory

--- aura-herbal-fixed/src/constants.ts
+++
@@ -1,6 +0,0 @@
-export const IMAGE_PATHS = {
-  LOGO: "/images/logo.png",
-  HERO_PRODUCT: "/images/hero-oil.jpeg",
-  HERBAL_PASTE: "/images/herbal-paste.png",
-  INGREDIENT_BOWL: "/images/ingredient-bowl.png",
-};
